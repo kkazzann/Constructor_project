55968ced871796fb16ff40b829fd3e86823d2caa
KamOri1 <iknowthis45@gmail.com>
2025-02-18 10:11:03 +0100

    feat(intro): add no_title function

--- components/intro.js
+++ components/intro.js
@@ -3,6 +3,34 @@ import { Space } from "./Space.js";
 
 export const Intro = isAllowToRender(({ title, paragraph, data, spaceClassName, color, align = "left" }) => {
   
+  if (title === 'no_title'){
+    return `
+  <table cellspacing="0" cellpadding="0" border="0" align="center" width="100%">
+    <tbody>
+      <tr>
+        <td>
+          ${Space({className: spaceClassName})}
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <table cellspacing="0" cellpadding="0" border="0" align="center" width="100%">
+            <tbody>
+              <tr>
+                <td align="${align}" >
+                  <span class="newsletterParagraph" style="color: ${color || "#000000"}">
+                    ${data ? data[1] : paragraph}
+                  </span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+    `;
+  }
   return `
   <table cellspacing="0" cellpadding="0" border="0" align="center" width="100%">
     <tbody>
